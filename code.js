98cb00be99c0f79952f16e1fd991bf261b4b25f2
allisonong <qishan.ong@gmail.com>
2021-01-05 09:17:23 -0800

    added beatles

---
+++ code.js
@@ -0,0 +1,79 @@
+contentElement = document.getElementById("content");
+albumsElement = document.getElementById("albums");
+milesElement = document.getElementById("miles");
+
+function clickFunction(obj) {
+	pLocal = document.getElementById("paragraph" + obj.id);
+	if (pLocal.style.display=="none") {
+		pLocal.style.display="";
+	} else {
+		pLocal.style.display = "none";
+	}
+}
+
+window.onload = function() {
+	// Set the albumsElement innerHTML
+	albumsElement.innerHTML = "Albums: " + runs.length; 
+
+	var totMiles=0;
+
+
+	// Parse the runs list and generate HTML elements
+	for (var i=0; i<runs.length; i++) {
+		run = runs[i];
+
+		//add up miles
+		totMiles = totMiles + parseFloat(run.miles);
+
+		// for each run object, add a new div
+		entryDiv = document.createElement("div");
+		entryDiv.id = i;
+		entryDiv.classList.add('entry');
+		//entryDiv.onclick = function() {clickFunction(this)};
+		contentElement.appendChild(entryDiv);
+
+		// img (in span)
+		img = document.createElement("img");
+		img.src = "images/" + run.img;
+		img.classList.add("images");
+		entryDiv.appendChild(img);
+
+		// header
+		entryHeader = document.createElement("h4");
+		entryHeader.innerHTML = run.date + "&nbsp;&nbsp;&nbsp; #" + run.rsnum + " " + run.title + " - " + run.artist + " (" + run.year + ")";
+		entryDiv.appendChild(entryHeader);
+
+		// span
+		imgQuoteDiv = document.createElement("div");
+		imgQuoteDiv.classList.add('quote');
+		entryDiv.appendChild(imgQuoteDiv);
+
+		// quote (in span)
+		imgQuoteDiv.innerHTML = imgQuoteDiv.innerHTML + "\"" + run.quote + "\"";
+
+		// paragraph
+		p = document.createElement("p");
+		p.innerHTML = run.text + "<br><br>";
+		p.id = "paragraph"+i;
+		//p.style.display = "none";
+
+		// span in p
+		milesSpan = document.createElement("span");
+		milesSpan.classList.add("miles");
+		milesSpan.innerHTML = "Miles run: " + run.miles;
+		p.appendChild(milesSpan);
+
+		// append p to entryDiv
+		entryDiv.appendChild(p);
+
+
+		if (i < (runs.length-1)) {
+			// also put a horizontal line
+			hr = document.createElement("hr");
+			content.appendChild(hr);
+		}
+	}
+
+	milesElement.innerHTML = "Miles: " + totMiles; 
+
+}
---
+++ runs.js
@@ -0,0 +1,50 @@
+runs = [
+	
+	{
+		"date":"11/26/2020",
+		"rsnum":"197",
+		"title":"Meet the Beatles",
+		"artist":"The Beatles",
+		"year":"1962",
+		"quote":"For Americans in the full grip of Beatlemania, this was the first album they could buy.",
+		"text":"I’m still running to the shortest albums I can because I have no endurance. Meet the Beatles was altogether pretty fun. It was easy listening and great for jogging slowly around the neighborhood, feeling fine. I think I can remember being a preschooler in Tokyo and singing “I want to hold your hand” in the car while holding my parents’ hands. This album is has a lot of good songs but the ones that really stick are “I wanna hold your hand”, “I saw her standing there”, and “All my loving’”. Listening in 2020, it was a little boring that the songs are all about making someone belong to you, or wanting to belong to someone else. I also couldn’t help but think that singing about 17 year olds and trying to seduce someone with the line “Little child little child, won't you dance with me? I'm so sad and lonely” is a little creepy. The description for this album on the Rolling Stone’s list makes me suspicious that it’s on the list more for its historical importance more than for the actual quality of the album.",
+		"miles":"2.42",
+		"img":"meet-the-beatles.jpg"
+	},
+
+	{
+		"date":"11/22/2020",
+		"rsnum":"402",
+		"title":"Expensive Shit",
+		"artist":"Fela Kuti and Africa 70",
+		"year":"1975",
+		"quote":"the Africa 70 horns blare out infectious riffs as peerless drummer Tony Allen keeps up an indefatigable shuffling pulse, while Fela calls out the “fools” who would “use your shit to put you for jail.",
+		"text":"Expensive Shit is the second shortest album on the list at 24 minutes and 13 seconds. This was my second run and running still does not feel enjoyable. From the get go my lungs burned and my legs felt like lead. It’s a good thing that this album was awesome! Not having much experience with afrobeat or funk (the genre according to Wikipedia), I feel like I lack the vocabulary to say why I liked it. All I can say is that the rhythm was great for running, it made me want to dance, the brass was amazing. It felt straight out of Cowboy Bebop, which is one of my favorite things. On the other hand my run was beautiful and I stopped to take some photos of fall. However, running is still hard, and I got passed by a lady pushing a stroller. Sigh.",
+		"miles":"1.8",
+		"img":"expensive-shit.jpg"
+	},
+
+	{
+		"date":"11/20/2020",
+		"rsnum":"410",
+		"title":"Wild Honey",
+		"artist":"The Beach Boys",
+		"year":"1967",
+		"quote":"In 24 concise but utterly winning minutes, they romp through a set of low-fi sunbaked melodies and R&B and soul homages — all suffused with warmth, sly hooks, and a sense of band unity.",
+		"text":"When I was a kid in Hong Kong my dad would give me his walkman to occupy me on hikes. My preferred CD at the time was some Beach Boys compilation album. The Beach Boys, as I knew them in my childhood, sang cheesy songs about California in high-pitched falsettos. As I started my run I was confused. This didn’t sound like the Beach Boys I’d known. But eventually my surprise wore off and I found myself enjoying the music. I even did the audio equivalent of a double-take when lyrics from “I’d Love Just Once to See You” gave me a little shock. This album sounded to me like old Rolling Stones or Beatles. On the very  last track, ‘Mama Says’, they return to the crooning falsettos and became The Beach Boys as I had known them. It turns out that is how I like them best. I chose this album to run to first because it is the shortest and I am weak. After 24 minutes of running I was wheezing. Damn.",
+		"miles":"1.96",
+		"img":"wild-honey.jpg"
+	}
+]
+
+//	{
+//		"date":"XX",
+//		"rsnum":"XX",
+//		"title":"XX",
+//		"artist":"XX",
+//		"year":"XX",
+//		"quote":"XX",
+//		"text":"XX",
+//		"miles":"XX",
+//		"img":"XX.jpg"
+//	},
